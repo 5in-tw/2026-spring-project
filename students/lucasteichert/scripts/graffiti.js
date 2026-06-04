0463f3a76ffb1f2cc1071ed594823ecac84016ad
lucas <lucasteichert.5in@gmail.com>
2026-06-04 15:54:43 +0800

    Working on eraser

--- students/lucasteichert/scripts/graffiti.js
+++ students/lucasteichert/scripts/graffiti.js
@@ -6,6 +6,8 @@ const surface = graffitiCanvas.getContext("2d");
 const cleanButton = document.getElementById("clean");
 const colorInput =document.getElementById ("color-input");
 const sizeInput =document.getElementById ("size-input");
+const toolSelect=document.getElementById("tool-select");
+console.log(toolSelect);
 /*
 * Graffiti style.
 */
@@ -50,7 +52,7 @@ function cleanCanvas() {
     surface.clearRect(0, 0, 400, 400);
 
 }
-cleanButton.addEventListener("click", cleanCanvas);
+
 
 let oldX= 0;
 let oldY = 0;
