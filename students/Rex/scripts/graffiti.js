64cb25068558dadbeeb9971a5da913f68f997b80
Rex <rexlee.5in@gmail.com>
2026-06-17 15:49:48 +0800

    Add CanvasEraser

--- students/Rex/scripts/graffiti.js
+++ students/Rex/scripts/graffiti.js
@@ -1,8 +1,11 @@
+import { CanvasEraser } from "../../../scripts/canvas-eraser.js";
 /*
 HTML elements
 */
 const graffitiCanvas = document.getElementById("my-graffiti");
 const surface = graffitiCanvas.getContext("2d");
+const eraser = new CanvasEraser(surface);
+
 const colorInput = document.getElementById("color-input");
 const sizeInput = document.getElementById("size-input");
 const toolSelect = document.getElementById("tool-select");
@@ -18,7 +21,6 @@ changeColor();
 colorInput.addEventListener("change", changeColor);
 
 function changeSize() {
-    console.log(sizeInput.value);
     surface.lineWidth = sizeInput.value;
 }
 changeSize();
@@ -27,7 +29,6 @@ sizeInput.addEventListener("change", changeSize);
 let tool;
 function changeTools() {
     tool = toolSelect.value;
-    console.log(tool);
 }
 changeTools();
 toolSelect.addEventListener("change",changeTools );
@@ -64,11 +65,10 @@ let oldY = 0;
 function graffiti(event) {
     const x = event.offsetX;
     const y = event.offsetY;
-    console.log(x, y, event.buttons);
     if (event.buttons > 0) {
         if(tool==="eraser"){
-            const width = sizeInput.value;
-            surface.clearRect(x-width/2, y-width/2, width, width);
+        const radius = sizeInput.value / 2;
+        eraser.circle(x, y, radius);
         }else{
         surface.beginPath();
         surface.moveTo(oldX, oldY);
