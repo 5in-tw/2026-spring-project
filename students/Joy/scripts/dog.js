f460152609bbe2b19b71c6b41f1296a97cb0dfa3
JohnAlbin <virtually.johnalbin@gmail.com>
2026-07-13 18:32:51 -0500

    Fix dog.js of Joy

--- students/Joy/scripts/dog.js
+++ students/Joy/scripts/dog.js
@@ -1,16 +1,16 @@
 import { ModelRotator } from "../../../scripts/model-rotator.js";
 
 const dog = new ModelRotator('dog-3d');
-const model = "/images/Duck.glb";
+const model = "./images/JoyDog.glb";
 const scale = 2;
 const position = {
-    x:5,
+    x:20,
     y:4,
-    z:-5,
+    z:10,
 };
 
 
-dog.load(model. scale);
+dog.load(model, scale);
 
 //dog.loadCube()
 dog.animate();
